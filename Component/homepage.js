70c5c9e0c8bf0c942e69fe6bfcbb55947781864c
ONG XIU QIN <116957205+ongxq@users.noreply.github.com>
2024-03-24 16:30:58 +0800

    Add files via upload

---
+++ Component/homepage.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const homepage = () => {
+  return (
+    <div className="homepg">
+      <div className="flower1-container">
+        <h1>Crop Cultivation</h1>
+        <p1>
+          Planting knowledge is the essence of Earth's salvation, fostering
+          reforestation, countering climate change, and safeguarding
+          biodiversity.
+        </p1>
+      </div>
+    </div>
+  );
+};
+
+export default homepage;
